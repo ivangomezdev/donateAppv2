826409a76728fb08446d1f84f4b93c9ccebbfc03
ivan <ivansangomez6@gmail.com>
2025-01-17 22:17:03 -0500

    v3

--- src/app/api/webhooks/mp/route.ts
+++ src/app/api/webhooks/mp/route.ts
@@ -1,41 +1,24 @@
 import { getPaymentById, WebhokPayload } from "@/lib/mercadopago";
 import { confirmPurchase } from "@/lib/purchases";
 
-export async function POST(request: Request) {
-  let body: WebhokPayload;
 
-  try {
-    body = await request.json();
-    console.log("Webhook received", body);
-  } catch (error) {
-    console.error("Error parsing JSON:", error);
-    return new Response("Invalid JSON", { status: 400 });
-  }
+export async function POST(request: Request, { params }) {
+ 
+  const body: WebhokPayload = await request.json();
+  console.log("Webhook received", body);
 
+console.log("Payment ID:", body.data.id);
   if (body.type === "payment") {
-    let mpPayment;
-
-    try {
-      mpPayment = await getPaymentById(body.data.id);
-    } catch (error) {
-      console.error("Error fetching payment from MercadoPago:", error);
-      return new Response("Error fetching payment", { status: 500 });
-    }
-
+    const mpPayment = await getPaymentById(body.data.id);
     if (mpPayment.status === "approved") {
       console.log(`Payment ${mpPayment.id} approved`);
+      
       const purchaseId = mpPayment.external_reference;
-
-      try {
-        await confirmPurchase(purchaseId);
-        console.log(`Purchase ${purchaseId} confirmed`);
-      } catch (error) {
-        console.error("Error confirming purchase:", error);
-        return new Response("Error confirming purchase", { status: 500 });
-      }
+      
+      await confirmPurchase(purchaseId);
     }
   }
 
-  // Responder siempre a MercadoPago
-  return new Response(JSON.stringify({ received: true }), { status: 200 });
+
+  return Response.json({ received: true });
 }
